c8ab341e2dada73f93e2c73660d7c953e53091de
Guillaume Catto <lowgravityisfun@gmail.com>
2015-10-28 21:08:53 +0100

    Added a test query for TheMovieDB wrapper.
    Added a test query for TheMovieDB wrapper.

--- app/src/main/java/com/github/skittlesdev/kubrick/Home.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/Home.java
@@ -3,11 +3,17 @@ package com.github.skittlesdev.kubrick;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.github.skittlesdev.kubrick.async.TestApiTask;
+
 public class Home extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         this.setContentView(R.layout.activity_home);
+
+        String apiKey = "0d1d0cc3c4aec9ca1c2c8c9e781a7ef1";
+        TestApiTask testApiTask = new TestApiTask(this);
+        testApiTask.execute(apiKey);
     }
 }
