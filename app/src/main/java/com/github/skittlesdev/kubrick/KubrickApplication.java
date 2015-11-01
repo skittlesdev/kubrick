4510bf0a41fe9c2caaa2d1703ed23322d0e04763
Hugo Caille <hugo.caille@isen-lille.fr>
2015-11-01 21:46:20 +0100

    Basic Parse integration

--- app/src/main/java/com/github/skittlesdev/kubrick/KubrickApplication.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/KubrickApplication.java
@@ -2,6 +2,7 @@ package com.github.skittlesdev.kubrick;
 
 import android.app.Application;
 import android.content.Context;
+import com.parse.Parse;
 import net.danlew.android.joda.JodaTimeAndroid;
 
 /**
@@ -15,6 +16,9 @@ public class KubrickApplication extends Application {
         super.onCreate();
         JodaTimeAndroid.init(this);
         KubrickApplication.sContext = this.getApplicationContext();
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "rej9XtKMoEPuEkULwnROypntYF5Bg9aKEQhQgYtr", "6kyMBoKJgKsMyQB72IvUWVq1UBZSJsXArxBIvMy3");
     }
 
     public static Context getContext() {
