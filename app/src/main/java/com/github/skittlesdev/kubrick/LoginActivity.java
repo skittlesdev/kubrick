66f3aa9390eef0269dd168a5ef7ef1a53798a014
Hugo Caille <hugo.caille@isen-lille.fr>
2015-11-02 02:01:41 +0100

    Login implementation

--- app/src/main/java/com/github/skittlesdev/kubrick/LoginActivity.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/LoginActivity.java
@@ -3,16 +3,36 @@ package com.github.skittlesdev.kubrick;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ((Button) findViewById(R.id.login)).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        String username = ((TextView) findViewById(R.id.username)).getText().toString();
+        String password = ((TextView) findViewById(R.id.password)).getText().toString();
 
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser parseUser, ParseException e) {
+                if (parseUser != null) {
+                    finish();
+                }
+                else {
+                    Toast.makeText(KubrickApplication.getContext(), "Login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
