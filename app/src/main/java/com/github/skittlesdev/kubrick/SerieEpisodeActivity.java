20ec698181b06d3b2898c5e83bc1e09c80aeaf2e
Guillaume Catto <lowgravityisfun@gmail.com>
2015-11-07 16:41:07 +0100

    Improved (a lot) the view for an episode given.
    Improved (a lot) the view for an episode given.

--- app/src/main/java/com/github/skittlesdev/kubrick/SerieEpisodeActivity.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/SerieEpisodeActivity.java
@@ -1,12 +1,19 @@
 package com.github.skittlesdev.kubrick;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import info.movito.themoviedbapi.model.tv.TvEpisode;
+import info.movito.themoviedbapi.model.tv.TvSeries;
 
 /**
  * Created by louis on 11/6/15.
@@ -20,13 +27,44 @@ public class SerieEpisodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_tv_episode);
 
+        this.setSupportActionBar((Toolbar) this.findViewById(R.id.toolBar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CardView cardView = (CardView) this.findViewById(R.id.overviewTVEpisodeCardView);
+        cardView.setBackgroundColor(Color.BLACK);
 
         ImageView poster = (ImageView) findViewById(R.id.tvEpisodePoster);
+        ImageView backdrop = (ImageView) findViewById(R.id.episodeBackDropPicture);
         TextView name = (TextView) findViewById(R.id.tvEpisodeName);
         TextView overview = (TextView) findViewById(R.id.tvEpisodeOverview);
 
         tvEpisode = (TvEpisode) getIntent().getSerializableExtra("tvEpisode");
         name.setText(tvEpisode.getName());
+        overview.setText(tvEpisode.getOverview());
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar.setTitleEnabled(true);
+        collapsingToolbar.setTitle(tvEpisode.getName());
+
+        if (tvEpisode.getImages() != null) {
+            if (tvEpisode.getImages().getBackdrops() != null && tvEpisode.getImages().getBackdrops().get(0) != null) {
+                Picasso.with(this)
+                        .load("http://image.tmdb.org/t/p/w500" + tvEpisode.getImages().getBackdrops().get(0))
+                        .placeholder(R.drawable.poster_default_placeholder)
+                        .error(R.drawable.poster_default_error)
+                        .fit()
+                        .into(backdrop);
+            }
+
+            if (tvEpisode.getImages().getPosters() != null && tvEpisode.getImages().getPosters().get(0) != null) {
+                Picasso.with(this)
+                        .load("http://image.tmdb.org/t/p/w500" + tvEpisode.getImages().getPosters().get(0))
+                        .placeholder(R.drawable.poster_default_placeholder)
+                        .error(R.drawable.poster_default_error)
+                        .fit()
+                        .into(poster);
+            }
+        }
 
     }
 
