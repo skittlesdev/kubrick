de89b7f7cc7f77ac4d9d97ebec11c513216f71c6
Hugo Caille <hugo.caille@isen-lille.fr>
2015-11-10 00:20:26 +0100

    Integrate fresco on related movies

--- app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarMoviesOverviewAdapter.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarMoviesOverviewAdapter.java
@@ -1,13 +1,13 @@
 package com.github.skittlesdev.kubrick.adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.github.skittlesdev.kubrick.R;
 
 import java.util.List;
@@ -25,7 +25,7 @@ public class SimilarMoviesOverviewAdapter extends RecyclerView.Adapter<SimilarMo
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         this.context = parent.getContext();
-        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.movies_layout, parent, false);
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorites_layout, parent, false);
         return new ViewHolder(layout);
     }
 
@@ -41,19 +41,15 @@ public class SimilarMoviesOverviewAdapter extends RecyclerView.Adapter<SimilarMo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private ImageView poster;
+        private SimpleDraweeView poster;
         private MovieDb item;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            this.poster = (ImageView) itemView.findViewById(R.id.moviePoster);
+            this.poster = (SimpleDraweeView) itemView.findViewById(R.id.moviePoster);
         }
         public void setposter(Context context) {
-            Glide.with(context)
-                    .load("http://image.tmdb.org/t/p/w500" + item.getPosterPath())
-                    .placeholder(R.drawable.poster_default_placeholder)
-                    .error(R.drawable.poster_default_error)
-                    .into(this.poster);
+            this.poster.setImageURI(Uri.parse("http://image.tmdb.org/t/p/w500" + item.getPosterPath()));
         }
 
         public void setItem(MovieDb item) {
