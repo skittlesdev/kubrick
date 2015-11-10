0097fa348866ef8abef213b3299d2ecb7e41b3d8
Hugo Caille <hugo.caille@isen-lille.fr>
2015-11-10 01:57:36 +0100

    Click listeners on related movies/series

--- app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarMoviesOverviewAdapter.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarMoviesOverviewAdapter.java
@@ -1,6 +1,7 @@
 package com.github.skittlesdev.kubrick.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.github.skittlesdev.kubrick.MediaActivity;
 import com.github.skittlesdev.kubrick.R;
 
 import java.util.List;
@@ -40,13 +42,14 @@ public class SimilarMoviesOverviewAdapter extends RecyclerView.Adapter<SimilarMo
         return this.movies.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private SimpleDraweeView poster;
         private MovieDb item;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.poster = (SimpleDraweeView) itemView.findViewById(R.id.moviePoster);
+            this.poster.setOnClickListener(this);
         }
         public void setposter(Context context) {
             this.poster.setImageURI(Uri.parse("http://image.tmdb.org/t/p/w500" + item.getPosterPath()));
@@ -54,6 +57,17 @@ public class SimilarMoviesOverviewAdapter extends RecyclerView.Adapter<SimilarMo
 
         public void setItem(MovieDb item) {
             this.item = item;
+        }
+
+        @Override
+        public void onClick(View view) {
+            Context context = view.getContext();
+
+            Intent intent = new Intent(context, MediaActivity.class);
+            intent.putExtra("MEDIA_TYPE", "movie");
+            intent.putExtra("MEDIA_ID", this.item.getId());
+
+            context.startActivity(intent);
         }
     }
 }
--- app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarSeriesOverviewAdapter.java
+++ app/src/main/java/com/github/skittlesdev/kubrick/adapters/SimilarSeriesOverviewAdapter.java
@@ -1,11 +1,14 @@
 package com.github.skittlesdev.kubrick.adapters;
 
+import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.github.skittlesdev.kubrick.MediaActivity;
 import com.github.skittlesdev.kubrick.R;
 
 import java.io.Serializable;
@@ -17,10 +20,10 @@ import java.util.Objects;
  * Created by Hugo Caille on 10/11/2015.
  */
 public class SimilarSeriesOverviewAdapter extends RecyclerView.Adapter<SimilarSeriesOverviewAdapter.ViewHolder> {
-    private List<HashMap<String, String>> results;
+    private List<HashMap<String, Object>> results;
 
     public SimilarSeriesOverviewAdapter(HashMap<String, Object> series) {
-        this.results = (List<HashMap<String, String>>) series.get("results");
+        this.results = (List<HashMap<String, Object>>) series.get("results");
     }
 
     @Override
@@ -39,18 +42,30 @@ public class SimilarSeriesOverviewAdapter extends RecyclerView.Adapter<SimilarSe
         return this.results.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private SimpleDraweeView poster;
-        private HashMap<String, String> item;
+        private HashMap<String, Object> item;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.poster = (SimpleDraweeView) itemView.findViewById(R.id.moviePoster);
+            this.poster.setOnClickListener(this);
         }
 
-        public void setItem(HashMap<String, String> item) {
+        public void setItem(HashMap<String, Object> item) {
             this.item = item;
             this.poster.setImageURI(Uri.parse("http://image.tmdb.org/t/p/w500" + item.get("poster_path")));
+        }
+
+        @Override
+        public void onClick(View view) {
+            Context context = view.getContext();
+
+            Intent intent = new Intent(context, MediaActivity.class);
+            intent.putExtra("MEDIA_TYPE", "tv");
+            intent.putExtra("MEDIA_ID", (int) this.item.get("id"));
+
+            context.startActivity(intent);
         }
     }
 }
